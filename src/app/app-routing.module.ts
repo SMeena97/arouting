45239af8aeb2aa56fa9f5944e5eecbe2a617c997
MeenaS <smeena.ece13@gmail.com>
2018-11-07 23:11:56 +0530

    to the github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 const routes: Routes = [];
 
 @NgModule({
@@ -8,3 +13,11 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const AppRoutes: Routes = [
+{ path: 'home', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'services', component: ServicesComponent },
+{ path: 'work', component: WorkComponent },
+{ path: 'contact', component: ContactComponent }
+];
+export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { ModuleWithProviders } from "@angular/core";
+import { AppRoutingModule, ROUTING } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	rootRouting,
+    ROUTING
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
